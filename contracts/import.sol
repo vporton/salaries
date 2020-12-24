37a3acd73bbee31a613b49eed795f560e8568547
Victor Porton <porton@narod.ru>
2020-12-24 23:19:16 +0200

    switched to Truffle

--- contracts/import.sol
+++ contracts/import.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.7.5;
+// HardHat workaround:
+// pragma solidity ^0.7.5;
+pragma solidity >=0.5.0;
 
 import "@gnosis.pm/conditional-tokens-contracts/contracts/BidOnAddresses.sol";
 import "@vporton/wrap-tokens/contracts/ERC1155OverERC20.sol";
