33690e1baa93263edad523e0e70a25d3eae2f6b8
Victor Porton <porton@narod.ru>
2020-12-25 05:41:53 +0200

    science donations deployment

--- migrations/02_deploy_science.js
+++ migrations/02_deploy_science.js
@@ -2,8 +2,11 @@ const myDeploy = require('../lib/default-deployer');
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 module.exports = async function(deployer, network, accounts) {
-  const uuid = '4a4552a6-4644-11eb-a830-3f3c92c66629';
-  const DefaultDAOInterface = artifacts.require("DefaultDAOInterface");
-  await deployProxy(DefaultDAOInterface, [], { deployer/*, initializer: 'store'*/ }); // FIXME: Owner?
-  await myDeploy(deployer, network, accounts, "SalaryWithDAO", (await DefaultDAOInterface.deployed()).address, `urn:uuid:${uuid}`);
+  { // Deploy science.
+    const uuid = '4a4552a6-4644-11eb-a830-3f3c92c66629';
+    const DefaultDAOInterface = artifacts.require("DefaultDAOInterface");
+    // FIXME: Add owner DAO.
+    await deployProxy(DefaultDAOInterface, []/*, { deployer: }*/);
+    await myDeploy(deployer, network, accounts, "SalaryWithDAO", (await DefaultDAOInterface.deployed()).address, `urn:uuid:${uuid}`);
+  }
 };
