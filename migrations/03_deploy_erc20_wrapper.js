76ed253bbd5d265baf0c4f46b736a30b1b478a7e
Victor Porton <porton@narod.ru>
2020-12-25 10:18:29 +0200

    deployment of test contracts

--- migrations/03_deploy_erc20_wrapper.js
+++
@@ -1,6 +0,0 @@
-const myDeploy = require('../lib/default-deployer');
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-
-module.exports = async function(deployer, network, accounts) {
-    await myDeploy(deployer, network, accounts, "ERC1155OverERC20");
-};
