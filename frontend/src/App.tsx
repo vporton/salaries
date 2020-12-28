5699e136ab8cc213e8962abf769bfb60f4f1aeeb
Victor Porton <porton@narod.ru>
2020-12-28 07:01:52 +0200

    bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -214,6 +214,7 @@ function App() {
       }
 
       updateInfo();
+      // eslint-disable-next-line
     }, [oracleId]);
 
     async function donate() {
@@ -441,7 +442,7 @@ function App() {
         <Route exact path="/" component={Pay}/>
         <Route path="/register" component={Register}/>
         <p>
-          <a target="_blank" href="https://github.com/vporton/donations">
+          <a rel="noreferrer" target="_blank" href="https://github.com/vporton/donations">
             <img src="img/GitHub-Mark-32px.png" width="32" height="32" alt="Source at GitHub"/>
           </a>
         </p>
