7de075d5e5e0ad8fc3d6616477f40415601ca10b
Victor Porton <porton@narod.ru>
2021-01-05 10:47:20 +0200

    deployment

--- migrations/02_deploy_climate.js
+++ migrations/02_deploy_climate.js
@@ -1,4 +1,5 @@
-const { myDeploy } = require('../lib/default-deployer');
+const fs = require('fs');
+const { myDeploy, updateAddress } = require('../lib/default-deployer');
 
 module.exports = async function(deployer, network, accounts) {
   const globalCommunityFund = accounts[0]; // FIXME
@@ -8,6 +9,25 @@ module.exports = async function(deployer, network, accounts) {
     "Non-retired carbon credits", "M-", "85d61e3c-48c3-11eb-8a97-ff3b9fcccdb2"
   );
   const uuid = '1bb89f0c-47ab-11eb-9b52-2bc780f5fac3';
-  const lock = await myDeploy(deployer, network, accounts, "Lock", `urn:uuid:${uuid}`);
-  // await climate.createOracle({contractAddress: carbon, tokenId: retiredCarbonTokenId})
+  const climate = await myDeploy(deployer, network, accounts, "Lock", `urn:uuid:${uuid}`);
+  const retiredCarbonTokenId = 1;
+  ({ logs } = await climate.createOracle({contractAddress: climate.address, tokenId: retiredCarbonTokenId}));
+  const oracleId = logs[0].args.oracleId;
+
+  // TODO: duplicate code
+  {
+    const addressesFileName = `donations-widget/public/addresses.json`;
+    let json;
+    try {
+        const text = fs.readFileSync(addressesFileName);
+        json = JSON.parse(text);
+    }
+    catch(_) {
+        json = {};  
+    }
+    updateAddress(json, network, 'data', {
+        climateOracleId: oracleId,
+    });
+    fs.writeFileSync(addressesFileName, JSON.stringify(json));
+  }
 };
--- migrations/03_deploy_science.js
+++ migrations/03_deploy_science.js
@@ -1,30 +1,30 @@
-const { myDeploy } = require('../lib/default-deployer');
+const fs = require('fs');
+const { myDeploy, updateAddress } = require('../lib/default-deployer');
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 module.exports = async function(deployer, network, accounts) {
   const uuid = '4a4552a6-4644-11eb-a830-3f3c92c66629';
   const DefaultDAOInterface = artifacts.require("DefaultDAOInterface");
-  const dao = deployer; // FIXME: Add owner DAO.
-  await deployProxy(DefaultDAOInterface, [], { deployer: dao });
+  const dao = accounts[0]; // FIXME: Add owner DAO.
+  await deployProxy(DefaultDAOInterface, [], { deployer }); // FIXME: deployer
   const science = await myDeploy(deployer, network, accounts, "SalaryWithDAO", (await DefaultDAOInterface.deployed()).address, `urn:uuid:${uuid}`);
-  // https://github.com/trufflesuite/truffle/issues/3645
-  // ({ logs } = await science.createOracle({ from: dao }));
-  // const oracleId = logs[0].args.oracleId;
+  ({ logs } = await science.createOracle({ from: dao }));
+  const oracleId = logs[0].args.oracleId;
 
-  // // TODO: duplicate code
-  // {
-  //   const addressesFileName = `frontend/public/addresses.json`;
-  //   let json;
-  //   try {
-  //       const text = fs.readFileSync(addressesFileName);
-  //       json = JSON.parse(text);
-  //   }
-  //   catch(_) {
-  //       json = {};  
-  //   }
-  //   updateAddress(json, network, 'data', {
-  //       scienceOracleId: oracleId,
-  //   });
-  //   fs.writeFileSync(addressesFileName, JSON.stringify(json));
-  // }
+  // TODO: duplicate code
+  {
+    const addressesFileName = `donations-widget/public/addresses.json`;
+    let json;
+    try {
+        const text = fs.readFileSync(addressesFileName);
+        json = JSON.parse(text);
+    }
+    catch(_) {
+        json = {};  
+    }
+    updateAddress(json, network, 'data', {
+        scienceOracleId: oracleId,
+    });
+    fs.writeFileSync(addressesFileName, JSON.stringify(json));
+  }
 };
