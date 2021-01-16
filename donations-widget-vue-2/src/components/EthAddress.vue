96a16cd1786d2f2e3aac354f805062b1fb86b66f
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-01-16 17:17:42 -0300

    Create Vue app

---
+++ donations-widget-vue-2/src/components/EthAddress.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span class="EthAddress">
+      <input
+        type="text"
+        maxLength="42"
+        size="50"
+        value="value"
+        :class="{ error: isAddressValid(props.value) }"
+        @change="onChange"
+      />
+    </span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'EthAddress',
+  props: ['onchange'],
+  data() {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onChange(value) {
+      this.value = value
+    },
+    isAddressValid(v) {
+      return window.web3.utils.isEthAddress(v)
+    }
+  }
+}
