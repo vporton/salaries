1578aecebadf0e16f0526c1a0d379228a5742e39
Victor Porton <porton@narod.ru>
2021-01-18 20:21:00 +0200

    binary package created

--- donations-widget-vue/build-binary.js
+++ donations-widget-vue/build-binary.js
@@ -1,16 +1,17 @@
 const fs = require('fs')
 const execSync = require('child_process').execSync;
 
-fs.rmdir('dist', { recursive: true })
-fs.mkdir('dist')
+fs.rmdir('dist-bin', { recursive: true }, () => {})
+fs.mkdir('dist-bin', () => {})
 
-execSync('yarn build')
+execSync('npx vue-cli-service build --target lib --dest dist-bin/Donate --name Donations src/views/Donate.vue')
+execSync('npx vue-cli-service build --target lib --dest dist-bin/Register --name Donations src/views/Register.vue')
 
-const jSrc = JSON.parse(fs.readFileSync('packages.json'))
-const jBin = JSON.parse(fs.readFileSync('packages-version.json'))
+const jSrc = JSON.parse(fs.readFileSync('package.json'))
+const jBin = JSON.parse(fs.readFileSync('package-binary.json'))
 
 jBin.version = jSrc.version
 
-fs.writeFileSync('dist/packages-version.json', JSON.serialize(jBin))
+fs.writeFileSync('dist-bin/package.json', JSON.stringify(jBin))
 
-execSync('cd dist && npm publish')
+execSync('cd dist-bin && npm login && npm publish --access public')
---
+++ donations-widget-vue/vue.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack')
+module.exports = {
+  filenameHashing: false,
+  configureWebpack: {
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1
+      })
+    ]
+  },
+  chainWebpack:
+    config => {
+      config.optimization.delete('splitChunks')
+    }
+}
