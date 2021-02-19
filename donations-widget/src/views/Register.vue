b68b8a6411d5600a9904c14eee0b54941a51b928
Victor Porton <porton@narod.ru>
2021-02-19 22:19:13 +0200

    text/design

--- donations-widget/src/views/Register.vue
+++ donations-widget/src/views/Register.vue
@@ -30,8 +30,8 @@
             Reset
           </button>
           {{' '}}
-          <button @click="showORCIDData" :style="{display: this.conditionId !== undefined ? 'inline' : 'none'}">
-            Show data for ORCID
+          <button @click="showRegistrationData" :style="{display: this.conditionId !== undefined ? 'inline' : 'none'}">
+            Show data for other sites
           </button>
         </span>
         <span :style="{display: noSuchConditionStyle, color: 'red'}"><br/>No such condition ID</span>
@@ -105,29 +105,54 @@
       }"
     >
       <div :style="{margin: '1ex'}">
-      <p>Now to make your salary calculation work (if you don't, you are not to receive a salary),
-        you need to enter the following data as a <strong>Funding</strong>
-        in <a target="_blank" href="https://orcid.org/signin">ORCID site</a>:</p>
-      <dl>
-        <dt>Funding type</dt><dd>Anything you want to enter</dd>
-        <dt>Title of funded project</dt><dd>Your full name (or anything, if you want to stay anonymous)</dd>
-        <dt>Funding agency display name</dt><dd><code>Future Salaries</code>
-          (Enter exactly this, with one space between words, no spaces before and after,
-          the same capitalization, etc.)</dd>
-        <dt>Funding agency display city</dt><dd><code>Ashkelon</code></dd>
-        <dt>Grant number</dt><dd><code ref="grantNumber"></code></dd>
-        <dt>Relationship</dt><dd><code>Self</code></dd>
-        <dt>Set visibility</dt>
-        <dd>
-          <code>everyone</code> (recommended for maximum salary) or
-          <code>trusted parties</code> (not recommended)
-        </dd>
-      </dl>
-      <p>You can leave the rest fields blank.</p>
-      <p>Note that for a given <q>oracle</q> like <code ref="grantNumberOracle"></code> you should have only one
-        <q>condition ID</q> <code ref="grantNumberCondition"></code>. (You can have multiple salary streams
-        for multiple oracles, but if for a single oracle you enter multiple condition IDs in ORCID site,
-        you may be even banned.)</p>
+        <p>Now to make your salary calculation work (if you don't, you are not to receive a salary),
+          you need to enter the following data.
+        <ul>
+          <li><a href="#orcidData">For scientists</a></li>
+          <li><a href="#githubData">For software developers</a></li>
+        </ul>
+        <h2 id="orcidData">ORCID</h2>
+        <p>If you are a scientist, enter the following data as <strong>Funding</strong>
+          in <a target="_blank" href="https://orcid.org/signin">ORCID site</a>:</p>
+        <dl>
+          <dt>Funding type</dt><dd>Anything you want to enter</dd>
+          <dt>Title of funded project</dt><dd>Your full name (or anything, if you want to stay anonymous)</dd>
+          <dt>Funding agency display name</dt><dd><code>Future Salaries</code>
+            (Enter exactly this, with one space between words, no spaces before and after,
+            the same capitalization, etc.)</dd>
+          <dt>Funding agency display city</dt><dd><code>Ashkelon</code></dd>
+          <dt>Grant number</dt><dd><code>{{`${networkname}/${oracleId}/${conditionId}`}}</code></dd>
+          <dt>Relationship</dt><dd><code>Self</code></dd>
+          <dt>Set visibility</dt>
+          <dd>
+            <code>everyone</code> (recommended for maximum salary) or
+            <code>trusted parties</code> (not recommended)
+          </dd>
+        </dl>
+        <p>You can leave the rest fields blank.</p>
+        <p>Note that for a given <q>oracle</q> like <code>{{`${networkname}/${oracleId}`}}</code> you should have only one
+          <q>condition ID</q> <code>{{conditionId}}</code>. (You can have multiple salary streams
+          for multiple oracles, but if for a single oracle you enter multiple condition IDs in ORCID site,
+          you may be even banned.)</p>
+        <h2 id="githubData">GitHub</h2>
+        <p>If you are a software developer, create a JSON file <code>.future-salaries/accounts.json</code>
+          (the file <code>accounts.json</code>  in the folder <code>.future-salaries</code>)
+          of the following content in the repository
+          <code>USER/USER</code> (where USER is your user name) with the following content:</p>
+        <pre>[
+  {
+    ethereumNetwork: '{{networkname}}',
+    oracleId: {{oracleId}},
+    conditionId: {{conditionId}}
+  }
+]</pre>
+        <p>You can have it enumerated through comma for multiple networks and/or oracle IDs.
+          (See any tutorial about JSON for the file format.)</p>
+        <p>Note that for a given combination of <code>ethereumNetwork</code> and <code>oracleId</code> you should have only one
+          <code>conditionId</code>. (You can have multiple salary streams for multiple oracles,
+          but if for a single oracle you enter multiple condition IDs, you may be even banned.)</p>
+        <p>It is OK to have the same <code>.future-salaries/accounts.json</code> file in multiple GitHub repositories.
+          Also please add the same file to other your Git hosts (BitBucket, GitLab, etc.)</p>
       </div>
     </vue-modal-2>
   </div>
@@ -499,7 +524,7 @@ export default {
           console.log('conditionId:', self.conditionId);
           self.updateRegisteredStatus(); // Call it even if self.conditionId didn't change.
           this.$emit('conditionCreated', self.conditionId);
-          self.showORCIDData();
+          self.showRegistrationData();
           for(let f of self.registerCallbacks) {
             f(self.conditionId);
           }
@@ -510,10 +535,7 @@ export default {
         }
       }
     },
-    showORCIDData() {
-      this.$refs.grantNumberOracle.textContent = `${this.networkname}/${this.oracleId}`;
-      this.$refs.grantNumberCondition.textContent = `${this.conditionId}`;
-      this.$refs.grantNumber.textContent = `${this.networkname}/${this.oracleId}/${this.conditionId}`;
+    showRegistrationData() {
       this.$vm2.open('registeredDialog');
     },
     updateSalary() {
