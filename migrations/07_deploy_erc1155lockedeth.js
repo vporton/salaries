899837c9ae8cb56d2ef0b84285d86e54aea24091
Victor Porton <porton@narod.ru>
2021-02-21 19:48:22 +0200

    bug fix

--- migrations/07_deploy_erc1155lockedeth.js
+++ migrations/07_deploy_erc1155lockedeth.js
@@ -4,8 +4,7 @@ const { myDeploy } = require('../lib/default-deployer');
 const uuid = '21ce1cbc-5bdf-11eb-8f58-a32affe2cc22';
 
 module.exports = async function(deployer, network, accounts) {
-    // FIXME: Before mainnet deploy uncomment.
-    //if(process.env.TEST) {
+    if(process.env.TEST) {
         await myDeploy(deployer, network, accounts, "ERC1155LockedETH", `urn:uuid:${uuid}`);
-    //}
+    }
 };
